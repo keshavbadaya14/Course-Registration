29d65a8ae1ff477c29ab86200fbbd14b4023d61e
keshavbadaya14 <keshav.badaya14@gmail.com>
2025-02-04 22:24:24 +0530

    added schedules

--- course_register/dashboard/admin.py
+++ course_register/dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import TimeTable, MessSchedule, ExamSchedule, BusSchedule
 # Register your models here.
+admin.site.register(TimeTable)
+admin.site.register(MessSchedule)
+admin.site.register(ExamSchedule)
+admin.site.register(BusSchedule)
--- course_register/dashboard/views.py
+++ course_register/dashboard/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from .models import TimeTable, MessSchedule, ExamSchedule, BusSchedule
 
-@login_required
 def student_dashboard(request):
-    student = request.user
-    timetable = TimeTable.objects.filter(student=student)
+    # If user is not authenticated, do not filter by student
+    if request.user.is_authenticated:
+        timetable = TimeTable.objects.filter(student=request.user)
+    else:
+        timetable = TimeTable.objects.all()  # Show empty or all timetables
+
     mess_schedule = MessSchedule.objects.all()
     exam_schedule = ExamSchedule.objects.all()
     bus_schedule = BusSchedule.objects.all()
